bc487d29a9a7ab3ef6857c52ec7462cc5050bd66
Ashfaq <syashfaq@gmail.comgit statuscleargit config --global user.name Ashfaqgit branch feature/initialcommitgit statuspwdgit config --global user.email syashfaq@gmail.com>
2023-02-20 10:05:37 +0530

    commited today

---
+++ Day01/regular-arrowFunction.js
@@ -0,0 +1,4 @@
+//Diffrenciate between regular function and arrow function
+//Regular function give access to calling function
+//Arrow function gives to their defining environment
+//The value of “this” keyword inside an arrow function depends on where the function was defined
